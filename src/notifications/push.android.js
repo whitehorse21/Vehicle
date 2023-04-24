d358986b719d4966977e34e5b53f61c2af3473d8
FullStackDev <whitehorse9021@gmail.com>
2023-04-24 15:17:38 -0500

    Fixed push notification issue

--- src/notifications/push.android.js
+++ src/notifications/push.android.js
@@ -1,48 +1,51 @@
-import {Notifications, Registered, PendingNotifications } from 'react-native-notifications';
+import {
+  Notifications,
+  Registered,
+  PendingNotifications,
+} from 'react-native-notifications';
 
 class PushNotification {
-	constructor() {
-		this.onNotification = null;
-		this.deviceToken = null;
+  constructor() {
+    this.onNotification = null;
+    this.deviceToken = null;
 
-		Notifications.registerRemoteNotifications();
-		Notifications.events().registerRemoteNotificationsRegistered((event) => {
-				// TODO: Send the token to my server so it could send back push notifications...
-				console.log("Device Token Received", event.deviceToken);
-				this.deviceToken = event.deviceToken;
-		});
-		// NotificationsAndroid.setRegistrationTokenUpdateListener((deviceToken) => {
-		// 	this.deviceToken = deviceToken;
-		// });
+    Notifications.registerRemoteNotifications();
+    Notifications.events().registerRemoteNotificationsRegistered(event => {
+      // TODO: Send the token to my server so it could send back push notifications...
+      console.log('Device Token Received', event.deviceToken);
+      this.deviceToken = event.deviceToken;
+    });
 
-		// NotificationsAndroid.setNotificationOpenedListener((notification) => {
-		// 	this.onNotification(notification);
-		// });
+    Notifications.events().registerNotificationOpened(
+      (notification, completion) => {
+        console.log('Notification opened by device user', notification.payload);
+        console.log(
+          `Notification opened with an action identifier: ${action.identifier} and response text: ${action.text}`,
+        );
+        completion();
+      },
+    );
 
-		Notifications.events().registerNotificationOpened((notification, completion) => {
-      console.log("Notification opened by device user", notification.payload);
-      console.log(`Notification opened with an action identifier: ${action.identifier} and response text: ${action.text}`);
-      completion();
-        });
-        
-        Notifications.events().registerNotificationReceivedBackground((notification, completion) => {
-      console.log("Notification Received - Background", notification.payload);
+    Notifications.events().registerNotificationReceivedBackground(
+      (notification, completion) => {
+        console.log('Notification Received - Background', notification.payload);
 
-      // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-      completion({alert: true, sound: true, badge: false});
-        });
-	}
+        // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+        completion({alert: true, sound: true, badge: false});
+      },
+    );
+  }
 
-	getDeviceToken() {
-		return this.deviceToken;
-	}
+  getDeviceToken() {
+    return this.deviceToken;
+  }
 
-	configure(params) {
-		this.onNotification = params.onNotification;
-		// NotificationsAndroid.refreshToken();
-		return Notifications.getInitialNotification();
-		// return PendingNotifications.getInitialNotification();
-	}
+  configure(params) {
+    this.onNotification = params.onNotification;
+    // NotificationsAndroid.refreshToken();
+    return Notifications.getInitialNotification();
+    // return PendingNotifications.getInitialNotification();
+  }
 }
 
 export default new PushNotification();
--- src/notifications/push.ios.js
+++ src/notifications/push.ios.js
@@ -1,58 +1,41 @@
-import { AppState } from 'react-native';
+import {AppState} from 'react-native';
 // import NotificationsIOS from 'react-native-notifications';
-import { Notifications } from 'react-native-notifications';
+import {Notifications} from 'react-native-notifications';
 class PushNotification {
-	constructor() {
-		this.onNotification = null;
-		this.deviceToken = null;
+  constructor() {
+    this.onNotification = null;
+    this.deviceToken = null;
 
-		// NotificationsIOS.addEventListener('remoteNotificationsRegistered', (deviceToken) => {
-		// 	this.deviceToken = deviceToken;
-		// });
+    // NotificationsIOS.requestPermissions();
+    Notifications.registerRemoteNotifications();
 
-		// NotificationsIOS.addEventListener('remoteNotificationsRegistrationFailed', (error) => {
-		// 	console.log(error)
-		// });
+    Notifications.events().registerRemoteNotificationsRegistered(event => {
+      // TODO: Send the token to my server so it could send back push notifications...
+      console.log('Device Token Received', event.deviceToken);
+      this.deviceToken = event.deviceToken;
+    });
 
-		// NotificationsIOS.addEventListener('notificationOpened', (notification, completion) => {
-		// 	const appState = AppState.currentState;
-		// 	if (appState && appState === 'background') {
-		// 		this.onNotification(notification);
-		// 	}
-		// 	completion();
-		// });
-	
-		// NotificationsIOS.requestPermissions();
-		Notifications.registerRemoteNotifications();
-		
-        Notifications.events().registerRemoteNotificationsRegistered((event) => {
-            // TODO: Send the token to my server so it could send back push notifications...
-            console.log("Device Token Received", event.deviceToken);
-            this.deviceToken = event.deviceToken;
-        });
-	
-		Notifications.events().registerNotificationOpened((notification, completion) => {
-	      // console.log("Notification opened by device user", notification.payload);
-	      // console.log(`Notification opened with an action identifier: ${action.identifier} and response text: ${action.text}`);
-	      	const appState = AppState.currentState;
-			if (appState && appState === 'background') {
-				this.onNotification(notification);
-			}
-	      	completion();
-	    });
-	}
+    Notifications.events().registerNotificationOpened(
+      (notification, completion) => {
+        const appState = AppState.currentState;
+        if (appState && appState === 'background') {
+          this.onNotification(notification);
+        }
+        completion();
+      },
+    );
+  }
 
-	getDeviceToken() {
-		return this.deviceToken;
-	}
+  getDeviceToken() {
+    return this.deviceToken;
+  }
 
-	async configure(params) {
-		this.onNotification = params.onNotification;
-		// const initial = Notifications.getInitialNotification();
-		const initial = await Notifications.getInitialNotification();
-		console.log('Initial ==== ', initial);
-		return Promise.resolve(initial);
-	}
+  async configure(params) {
+    this.onNotification = params.onNotification;
+    const initial = await Notifications.getInitialNotification();
+    console.log('Initial ==== ', initial);
+    return Promise.resolve(initial);
+  }
 }
 
 export default new PushNotification();
